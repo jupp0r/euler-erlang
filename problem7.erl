eb93be655d6d75cb6e8ac28bdd9eef4ebdb97f44
Jupp Müller <jupp0r@gmail.com>
2012-02-08 09:46:46 +0100

    solved problem 7

---
+++ problem7.erl
@@ -0,0 +1,31 @@
+-module(problem7).
+-include_lib("eunit/include/eunit.hrl").
+-import(euler_helper,[prime/1]).
+-export([problem7/0]).
+
+
+last_prime(N) ->
+    last_prime(N,1,2).
+
+last_prime(N,K,X) ->
+    XisPrime = prime(X),
+    if
+        XisPrime ->
+            if
+                N == K ->
+                    X;
+                true ->
+                    last_prime(N,K+1,X+1)
+            end;
+        true ->
+            last_prime(N,K,X+1)
+    end.
+
+problem7() ->
+    last_prime(10001).
+
+%% tests
+last_prime_test_() ->
+    [ ?_assertEqual(last_prime(1),2),
+      ?_assertEqual(last_prime(6),13) ].
+
