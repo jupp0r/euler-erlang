ab7a7a4253f44e165d89e07cb94c6a44a64a2f7d
Jupp Müller <jupp0r@gmail.com>
2012-02-19 15:43:07 +0100

    solved problem 26

---
+++ problem26.erl
@@ -0,0 +1,74 @@
+-module(problem26).
+-include_lib("eunit/include/eunit.hrl").
+-import(euler_helper,[int_pow/2]).
+-export([problem26/0]).
+
+div_list(N) ->
+    div_list(N,N).
+
+div_list(N,K) ->
+    integer_to_list(int_pow(10,2*K) div N).
+
+loop_len(Str) ->
+    Match = re:run(Str,"^(([0-9])|\\2+)*?(([0-9]+?)\\4+)[0-9]*$",[{capture,[4]}]),
+    case Match of
+        {match,[{_,Len}]} ->
+            Len;
+        nomatch ->
+            0
+    end.
+
+div_len(N) ->
+    {N,loop_len(div_list(N))}.
+
+problem26() ->
+    LenList = [div_len(X) || X <- lists:seq(2,999)],
+    lists:foldr(
+      fun ({X,XVal},{Max,MaxVal}) ->
+              if
+                  XVal > MaxVal ->
+                      {X,XVal};
+                  true -> {Max,MaxVal}
+              end
+      end,
+      {error,0}, LenList).
+
+%% tests
+
+div_list_double_test_() ->
+    [
+     ?_assertEqual("50",div_list(2,1)),
+     ?_assertEqual("33",div_list(3,1)),
+     ?_assertEqual("3333",div_list(3,2)),
+     ?_assertEqual("2500",div_list(4,2))
+    ].
+
+div_list_test_() ->
+    [
+     ?_assertEqual("5000",div_list(2)),
+     ?_assertEqual("2000000000",div_list(5)),
+     ?_assertEqual("333333",div_list(3))
+    ].
+
+loop_len_test_() ->
+    [
+     ?_assertEqual(1,loop_len("33333")),
+     ?_assertEqual(2,loop_len("121212")),
+     ?_assertEqual(0,loop_len("123456890")),
+     ?_assertEqual(0,loop_len(""))
+    ].
+
+div_len_test_() ->
+    [
+     ?_assertEqual({3,1},div_len(3)),
+     ?_assertEqual({7,6},div_len(7)),
+     ?_assertEqual({6,1},div_len(6)),
+     ?_assertEqual({37,3},div_len(37)),
+     ?_assertEqual({185,3},div_len(185)),
+     ?_assertEqual({11,2},div_len(11)),
+     ?_assertEqual({81,9},div_len(81)),
+     ?_assertEqual({17,16},div_len(17)),
+     ?_assertEqual({19,18},div_len(19)),
+     ?_assertEqual({29,28},div_len(29)),
+     ?_assertEqual({97,96},div_len(97))
+    ].
