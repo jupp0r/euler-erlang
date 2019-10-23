aedff6339da6a8cb755e4a2d58a753388632c7aa
Jupp Mueller <jupp0r@gmail.com>
2019-10-23 10:55:33 -0700

    Update problem67.erl

--- problem67.erl
+++ problem67.erl
@@ -1,7 +1,7 @@
 -module(problem67).
 -include_lib("eunit/include/eunit.hrl").
 -import(euler_helper,[triangle_seq/1,longest_path/2,read_triangular_graph_data/1]).
--export([problem67/0]).
+-expot([problem67/0]).
 
 problem67() ->
     G = read_triangular_graph_data("problem67.txt"),
